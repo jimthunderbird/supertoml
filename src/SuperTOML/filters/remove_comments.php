4c54b4c54fe329213c0da06ab1a1bb623d6f5fc0
Jim Huang <jim.huang@glu.com>
2020-04-19 20:45:14 -0700

    better naming convention for line

--- src/SuperTOML/filters/remove_comments.php
+++ src/SuperTOML/filters/remove_comments.php
@@ -2,8 +2,8 @@
 return function($content) {
     //remove all comments
     $lines = \explode("\n", $content);
-    $lines = \array_map(function($match) {
-        return \preg_replace("/\/\*[\s\S]*?\*\/|([^:]|^)\#.*|([^:]|^)\/\/.*/", "", $match);
+    $lines = \array_map(function($line) {
+        return \preg_replace("/\/\*[\s\S]*?\*\/|([^:]|^)\#.*|([^:]|^)\/\/.*/", "", $line);
     }, $lines);
 
     $lines = \array_filter($lines, function($line) {
